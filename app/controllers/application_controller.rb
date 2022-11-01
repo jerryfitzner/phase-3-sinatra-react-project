4080d4edb8d9937561b0288fe4ce4238abe4804e
Jerry Fitzner <jfitzner1@gmail.com>
2022-10-31 20:24:33 -0400

    Updated Students Post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,22 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/students" do
-    students = Student.all
+    students = Student.all.order(:first_name)
     students.to_json(include: [:donors])
   end
 
-  get "/donors" do
-    donors = Donor.all
-    donors.to_json(include: [:student])
+  post "/students" do
+    student = Student.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      age: params[:age]
+    )
+    student.to_json
   end
+
+  # get "/donors" do
+  #   donors = Donor.all
+  #   donors.to_json(include: [:student])
+  # end
 
 end
